38506d4de0923967fd0bc412f52abc76caf16daf
Rafa Linares Molina <rafa.linares.molina@gmail.com>
2020-09-21 09:47:07 +0200

    Initial commit with first version of the calculator

---
+++ js/app.js
@@ -0,0 +1,125 @@
+
+const auxResElement = document.getElementById("auxResult");
+const newResultElement = document.getElementById("finalResult");
+const operator = document.getElementById("operator");
+
+newResultElement.innerHTML = "";
+auxResElement.innerHTML = "";
+operator.innerHTML = ""
+let isANewCalc = false;
+
+const validOperations = ["+", "-", "*", "/"];
+
+const pushedButton = (value) => {
+  if (validOperations.includes(value)) {
+		makeOperation();
+    operator.innerHTML = value;
+    if (auxResElement.innerHTML.length !== 0) {
+      makeOperation();
+    } else {
+      auxResElement.innerHTML = newResultElement.innerHTML;
+      newResultElement.innerHTML = "";
+    }
+  } else {
+    addNumber(value);
+  }
+};
+
+const clearScreen = () => {
+	newResultElement.innerHTML = "";
+	auxResElement.innerHTML = "";
+	operator.innerHTML = "";
+}
+
+const addNumber = (number) => {
+	if(newResultElement.innerHTML.length === 0){
+		newResultElement.innerHTML = number;
+	} else {
+		if(isANewCalc){
+			isANewCalc = false;
+			newResultElement.innerHTML = "";
+			newResultElement.innerHTML += number;
+		}else {
+			newResultElement.innerHTML += number;
+		}
+	}
+};
+
+const isAuxEmpty = () => {
+  return auxResElement.innerHTML.length === 0;
+};
+
+const isNumberEmpty = () => {
+  return newResultElement.innerHTML.length === 0;
+};
+
+const makeSum = () => {
+  if (isAuxEmpty()) {
+    auxResElement.innerHTML = newResultElement.innerHTML;
+    newResultElement.innerHTML = "";
+  } else if (!isNumberEmpty()) {
+    auxResElement.innerHTML = parseFloat(auxResElement.innerHTML) + parseFloat(newResultElement.innerHTML);
+    newResultElement.innerHTML = "";
+  }
+};
+
+const makeRes = () => {
+  if (isAuxEmpty()) {
+    auxResElement.innerHTML = newResultElement.innerHTML;
+    newResultElement.innerHTML = "";
+  } else if (!isNumberEmpty()) {
+    auxResElement.innerHTML = parseFloat(auxResElement.innerHTML) - parseFloat(newResultElement.innerHTML);
+    newResultElement.innerHTML = "";
+  }
+};
+
+const makeMul = () => {
+  if (isAuxEmpty()) {
+    auxResElement.innerHTML = newResultElement.innerHTML;
+    newResultElement.innerHTML = "";
+  } else if (!isNumberEmpty()) {
+    auxResElement.innerHTML = parseFloat(auxResElement.innerHTML) * parseFloat(newResultElement.innerHTML);
+    newResultElement.innerHTML = "";
+  }
+};
+
+const makeDiv = () => {
+  if (isAuxEmpty()) {
+    auxResElement.innerHTML = newResultElement.innerHTML;
+    newResultElement.innerHTML = "";
+  } else if (!isNumberEmpty()) {
+    auxResElement.innerHTML = parseFloat(auxResElement.innerHTML) / parseFloat(newResultElement.innerHTML);
+    newResultElement.innerHTML = "";
+  }
+};
+
+const obtainResult = () => {
+  if (!isAuxEmpty() && !isNumberEmpty()) {
+    makeOperation();
+		newResultElement.innerHTML = auxResElement.innerHTML;
+		auxResElement.innerHTML = "";
+		operator.innerHTML = ""
+		isANewCalc = true;
+  }
+};
+
+const makeOperation = () => {
+  switch (operator.innerHTML) {
+    case "+":
+      makeSum();
+      break;
+    case "-":
+      makeRes();
+      break;
+    case "*":
+      makeMul();
+      break;
+    case "/":
+      makeDiv();
+      break;
+    case "c":
+      newResultElement.innerHTML = "";
+      auxResElement.innerHTML = "";
+      break;
+  }
+};
